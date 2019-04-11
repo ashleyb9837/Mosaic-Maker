ce0ecc393b3f0dabc2f9a57d1b0870d369d792e9
ashleyb9837 <48296899+ashleyb9837@users.noreply.github.com>
2019-04-11 17:41:15 +0100

    Add files via upload

---
+++ flaskmosaic.py
@@ -0,0 +1,57 @@
+print ( )
+from flask import Flask, request, render_template, session
+import re
+import random
+app = Flask(__name__)
+
+
+@app.route("/",methods=['GET', 'POST'] )
+def main():
+    randomcolourr2 = lambda: random.randint(0, 255)
+    displaycolu2 = ('#%02X%02X%02X' % (randomcolourr2(), randomcolourr2(), randomcolourr2()))
+
+
+    color = "FFFFFF"
+    new_color = request.form.get('color', '')
+    if re.search(r'^[0-9A-F]{6}$', new_color):
+        color = new_color
+
+#        randomcolourr1 = lambda: random.randint(0, 255)
+#        displaycolu1 = ('#%02X%02X%02X' % (randomcolourr1(), randomcolourr1(), randomcolourr1()))
+    print(new_color, displaycolu2)
+    return render_template('main.html', color=color, displaycolu2=displaycolu2)
+
+# Python program to convert decimal to binary,
+# octal and hexadecimal
+
+# Function to convert decimal to binary
+def decimal_to_binary(dec):
+	decimal = int(dec)
+
+	# Prints equivalent decimal
+	print(decimal, " in Binary : ", bin(decimal))
+
+# Function to convert decimal to octal
+def decimal_to_octal(dec):
+	decimal = int(dec)
+
+	# Prints equivalent decimal
+	print(decimal, "in Octal : ", oct(decimal))
+
+# Function to convert decimal to hexadecimal
+def decimal_to_hexadecimal(dec):
+	decimal = int(dec)
+
+	# Prints equivalent decimal
+	print(decimal, " in Hexadecimal : ", hex(decimal))
+
+# Driver program
+dec = 32
+decimal_to_binary(dec)
+decimal_to_octal(dec)
+decimal_to_hexadecimal(dec)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
